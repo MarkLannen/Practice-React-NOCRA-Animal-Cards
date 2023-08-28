7a3a30af7e5c2ee4182e8eb213ecc5eeaaa435cf
Mark.Lannen <mark.lannen@mso.umt.edu>
2023-08-28 17:03:53 -0600

    add AnimalShow component. Add random animal to animals array

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-
 import React from "react";
+import { useState } from "react";
+
+const getRandomAnimal = () => {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+};
+
 const App = () => {
-    return <div>Hello World! This is a React App made from Scratch! Animal Cards</div>
+  const [animals, setAnimals] = useState([]);
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}> Animal</button>
+      <div>{animals}</div>
+    </div>
+  );
 };
 
 export default App;
